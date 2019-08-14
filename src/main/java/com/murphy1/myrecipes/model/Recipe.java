47215d17f49494161e6fae5225cf1006ae016ccd
Stephen Murphy <smurphy54321@gmail.com>
2019-08-14 12:21:19 -0400

    Added the Ingredient Entity. Added and updated JPA mappings and annotations. Closes #4

--- src/main/java/com/murphy1/myrecipes/model/Recipe.java
+++ src/main/java/com/murphy1/myrecipes/model/Recipe.java
@@ -1,6 +1,7 @@
 package com.murphy1.myrecipes.model;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -19,6 +20,9 @@ public class Recipe {
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
 
     @Lob
     private Byte[] image;
@@ -93,6 +97,14 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 
     public Byte[] getImage() {
